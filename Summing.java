46c6d73fc2f3cb10ff730db9ea0a81c907b2e557
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-03 15:56:27 +0530

    Add files via upload

---
+++ Summing.java
@@ -0,0 +1,14 @@
+package JavaPrograms;
+
+public class Summing 
+{
+	public static void main(String[] args)
+	{
+		System.out.print("The sum of the three nums:");
+		int n1=2;
+		int n2=3;
+		int n3=4;
+		int sum=n1+n2+n3;
+		System.out.println(sum);
+	}
+}
