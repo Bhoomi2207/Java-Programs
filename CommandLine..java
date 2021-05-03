96d73d6c65df2987e80c94268195f3769f335859
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-03 15:41:12 +0530

    Create CommandLine..java

---
+++ CommandLine..java
@@ -0,0 +1,17 @@
+//CommandLine.java
+public class CommandLine 
+{
+	public static void main(String[] args)
+	{
+		int count, i=0;
+		String string;
+		count = args.length;
+		System.out.println("number of arguments: " + count);
+		while(i < count)
+		{
+			string = args[i];
+			i = i + 1;
+			System.out.println(i + ":" + "Hello "+string+"!");
+		}
+	}
+}
